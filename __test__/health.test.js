3afb12fc87b87bf6ea3ea44dbfdcdecbad2c349e
alejogs4 <alejogs4@gmail.com>
2019-08-04 09:15:04 -0500

    Agrega primera pagina

--- __test__/health.test.js
+++ __test__/health.test.js
@@ -1,13 +1,13 @@
-import { shallow } from 'enzyme';
+import { shallow } from "enzyme"
 
 describe('Health test', () => {
 	it('Should works without problems', () => {
-		expect(2).toBe(2);
-	});
+		expect(2).toBe(2)
+	})
 
 	it('Should tests components properly', () => {
 		const Component = () => <h1>Hello</h1>
-		const wrapper = shallow(<Component />);
-		expect(wrapper.find('h1')).toHaveLength(1);
-	});
-});
+		const wrapper = shallow(<Component />)
+		expect(wrapper.find('h1')).toHaveLength(1)
+	})
+})
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default () => (
+	<>
+		<Head>
+			<title>Verdulistas</title>
+		</Head>
+		<h1>Verdulistas</h1>
+	</>
+)
