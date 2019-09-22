ae63d66c3ecc799bfd02ad77c3271b7e189ca3d9
maugarbru <mauriciogarzonb@gmail.com>
2019-09-22 14:30:37 -0500

    Cambios visuales en catálogo

--- pages/catalogue.vue
+++ pages/catalogue.vue
@@ -1,94 +1,135 @@
 <template>
-  <v-flex>
-    <v-layout>
-      <!-- Filtros -->
-      <v-navigation-drawer permanent>
-        <v-toolbar flat>
-          <v-list class="secondary">
-            <v-list-tile>
-              <v-list-tile-title class="title">
-                <strong>
-                  [
-                  <v-icon color="warning">build</v-icon>WIP]
-                  <v-icon>policy</v-icon>Filtros
-                </strong>
-              </v-list-tile-title>
-            </v-list-tile>
-          </v-list>
-        </v-toolbar>
-        <v-list dense class="pt-0">
-          <v-list-tile v-for="item in items" :key="item.title">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-          <v-divider></v-divider>
-          <div class="secondary">
-            <v-btn @click="limpiarFiltros()" small outline>
-              <v-icon>restore</v-icon>Limpiar filtros
-            </v-btn>
-          </div>
-        </v-list>
-      </v-navigation-drawer>
-
-      <!-- Catálogo -->
-      <v-flex xs12>
-        <v-card>
-          <v-card-title class="primary">
-            <v-icon left large>widgets</v-icon>
-            <h1>
-              <strong>CATÁLOGO</strong>
-            </h1>
-          </v-card-title>
-          <v-card-text class="secondary">
-            <v-progress-linear color="info" indeterminate v-if="loading"></v-progress-linear>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex v-for="product in products" :key="product.id">
-                  <v-card>
-                    <v-img :src="product.image" width="220px">
-                      <v-chip dark class="warning" @click="showProduct(product)">
-                        <strong>{{product.name}}</strong>
-                      </v-chip>
-                    </v-img>
-                    <v-divider></v-divider>
-                    <v-card-actions>
-                      <p>Precio: ${{product.price}}</p>
-                      <v-spacer></v-spacer>
-                      <v-btn icon outline @click="showProduct(product)">
-                        <v-icon>zoom_in</v-icon>
-                      </v-btn>
-                      <v-btn
-                        v-if="logged"
-                        icon
-                        outline
-                        color="accent"
-                        @click="addProductToCart(product)"
-                      >
-                        <v-icon>add_shopping_cart</v-icon>
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-text>
-        </v-card>
-
-        <!-- Botón carrito -->
-      </v-flex>
-      <v-flex xs1>
+  <div>
+    <!-- Catálogo -->
+    <v-flex xs12>
+      <v-toolbar class="auto" sticky color="primary">
+        <v-btn color="black" outline @click.stop="drawer2 = !drawer2">
+          <v-icon dark>youtube_searched_for</v-icon>Filtros
+        </v-btn>
+        <v-spacer></v-spacer>
         <v-btn v-if="logged" dark color="info" @click.stop="drawer = !drawer">
           <v-avatar v-if="quantity > 0" size="25px" color="warning">
             <span class="white--text">{{ quantity }}</span>
           </v-avatar>
           <v-icon dark>shopping_cart</v-icon>Ver carrito
         </v-btn>
-      </v-flex>
+      </v-toolbar>
+      <v-card>
+        <v-card-text class="secondary">
+          <v-progress-linear color="info" indeterminate v-if="loading"></v-progress-linear>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex v-for="product in products" :key="product.id">
+                <v-card>
+                  <v-img :src="product.image" width="300px">
+                    <v-chip dark class="warning" @click="showProduct(product)">
+                      <strong>{{product.name}}</strong>
+                    </v-chip>
+                  </v-img>
+                  <v-divider></v-divider>
+                  <v-card-actions>
+                    <p>Precio: ${{product.price}}</p>
+                    <v-spacer></v-spacer>
+                    <v-btn icon outline @click="showProduct(product)">
+                      <v-icon>zoom_in</v-icon>
+                    </v-btn>
+                    <v-btn
+                      v-if="logged"
+                      icon
+                      outline
+                      color="accent"
+                      @click="addProductToCart(product)"
+                    >
+                      <v-icon>add_shopping_cart</v-icon>
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+
+    <!-- DESCRIPCION DE PRODUCTO -->
+    <v-container>
+      <v-dialog v-model="dialog" scrollable width="400" transition="scale-transition">
+        <v-card>
+          <v-img :src="selected.image" width="400">
+            <v-chip outline label dark>
+              <v-avatar>
+                <v-icon>label</v-icon>
+              </v-avatar>
+              <strong>{{selected.code}}</strong>
+            </v-chip>
+          </v-img>
+          <br />
+          <v-card-title class="warning">
+            <h1>{{selected.name}}</h1>
+          </v-card-title>
+          <v-card-text>{{selected.description}}</v-card-text>
+          <v-card-actions>
+            <v-chip color="info" label outline dark>
+              <v-avatar>
+                <v-icon>local_atm</v-icon>
+              </v-avatar>
+              <strong>Precio: ${{selected.price}}</strong>
+            </v-chip>
+            <v-spacer></v-spacer>
+            <v-btn v-if="logged" color="accent" @click="addProductToCart(selected)">
+              Añadir al carrito
+              <v-icon>add_shopping_cart</v-icon>
+            </v-btn>
+            <v-btn v-if="!logged" color="error" to="/login">
+              <v-icon left>person_add_disabled</v-icon>Iniciar sesión
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-container>
+
+    <!-- FILTROS -->
+    <v-layout wrap style="height: 200px;">
+      <v-navigation-drawer v-model="drawer2" absolute left temporary>
+        <v-list class="pa-1">
+          <v-list-tile avatar tag="div" class="warning">
+            <v-icon left>youtube_searched_for</v-icon>
+
+            <v-list-tile-content>
+              <v-list-tile-title class="white--text">Filtros</v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-btn icon @click.stop="drawer2 = !drawer2">
+                <v-icon>exit_to_app</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+        <v-list two-line dense class="secondary">
+          <template v-for="item in items">
+            <v-list-tile :key="item.title" avatar>
+              <v-list-tile-avatar>
+                <v-icon>{{item.icon}}</v-icon>
+              </v-list-tile-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.title"></v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+        <v-divider light></v-divider>
+        <v-list-tile>
+          <v-btn outline @click="cleanAllCart()">
+            Limpiar
+            <v-icon>restore</v-icon>
+          </v-btn>
+          <v-btn color="info" @click="orderPrice()">
+            <v-icon>policy</v-icon>Aplicar
+          </v-btn>
+        </v-list-tile>
+      </v-navigation-drawer>
     </v-layout>
 
     <!-- CARRITO -->
@@ -143,7 +184,7 @@
         </v-list>
         <v-divider light></v-divider>
         <v-list-tile>
-          <v-btn outline color="success" :disabled="quantity == 0" @click="orderPrice()">
+          <v-btn color="success" :disabled="quantity == 0" @click="orderPrice()">
             <v-icon>shop</v-icon>Comprar
           </v-btn>
           <v-btn outline @click="cleanAllCart()">
@@ -153,41 +194,6 @@
         </v-list-tile>
       </v-navigation-drawer>
     </v-layout>
-
-    <!-- DESCRIPCION DE PRODUCTO -->
-    <v-dialog v-model="dialog" max-width="400">
-      <v-card>
-        <v-img :src="selected.image" contain>
-          <v-chip outline label dark>
-            <v-avatar>
-              <v-icon>label</v-icon>
-            </v-avatar>
-            <strong>{{selected.code}}</strong>
-          </v-chip>
-        </v-img>
-        <br />
-        <v-card-title class="warning">
-          <h1>{{selected.name}}</h1>
-        </v-card-title>
-        <v-card-text>{{selected.description}}</v-card-text>
-        <v-card-actions>
-          <v-chip color="info" label outline dark>
-            <v-avatar>
-              <v-icon>local_atm</v-icon>
-            </v-avatar>
-            <strong>Precio: ${{selected.price}}</strong>
-          </v-chip>
-          <v-spacer></v-spacer>
-          <v-btn v-if="logged" color="accent" @click="addProductToCart(selected)">
-            Añadir al carrito
-            <v-icon>add_shopping_cart</v-icon>
-          </v-btn>
-          <v-btn v-if="!logged" color="error" to="/login">
-            <v-icon left>person_add_disabled</v-icon>Iniciar sesión
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
 
     <!-- CONFIRMACIÓN DE COMPRA -->
     <v-dialog v-model="dialogCompra" scrollable max-width="550">
@@ -283,7 +289,7 @@
         </v-alert>
       </v-card>
     </v-dialog>
-  </v-flex>
+  </div>
 </template>
 
 <script>
@@ -308,6 +314,7 @@ export default {
       dialogCompra: false,
       loading: null,
       drawer: null,
+      drawer2: null,
       items: [
         { title: "Nombre", icon: "keyboard" },
         { title: "Precio", icon: "attach_money" }
@@ -486,8 +493,8 @@ export default {
         this.cart = [];
         this.orderText = "Pedido generado correctamente, compruebe su correo";
         this.order = true;
-        setTimeout(() => this.order = false, 1500);
-        setTimeout(() => this.dialogCompra = false, 2500)
+        setTimeout(() => (this.order = false), 1500);
+        setTimeout(() => (this.dialogCompra = false), 2500);
       });
     },
     getUser() {
@@ -500,10 +507,10 @@ export default {
       }
     },
     orderPrice() {
-      this.address = "",
-      this.phone = "",
-      this.valid = false,
-      this.totalPrice = 0;
+      (this.address = ""),
+        (this.phone = ""),
+        (this.valid = false),
+        (this.totalPrice = 0);
       this.cart.forEach(element => {
         this.totalPrice += element.product.price * element.quantity;
       });
